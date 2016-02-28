f1f8958d7b4f709a8e5c26c85f6bb3d9fce0d2a3
Elyse Kolker Gordon <elyse1213+github@gmail.com>
2016-02-27 17:15:50 -0800

    init commit divi child theme for burnt toast food blog

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+}
+add_action('et_header_top', 'add_tag_line');
+function add_tag_line() {
+     echo '<div class="tag-line">tag line. goes here. yeah a tag line.</div>' . "\n";
+}
+?>
